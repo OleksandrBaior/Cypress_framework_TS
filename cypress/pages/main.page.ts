488c3d4f65c9fd88e2f0488405194bd888b9ca5e
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-17 22:20:33 +0200

    create TC_4

--- cypress/pages/main.page.ts
+++ cypress/pages/main.page.ts
@@ -1,6 +1,14 @@
 class MainPage {
-  elements = {
-
+  contactWithUsElements = {
+    tiltleSection: () => cy.get('.c-plaCa > .c-gdniMJ > .c-fNZqWL'),
+    tiltle: () => cy.get('.c-plaCa > .c-gdniMJ > .c-fKwEGa'),
+    subTitle: () => cy.get('.c-plaCa > .c-gdniMJ > .c-PJLV-ghYBfS-lead-true'),
+    emailInput: () => cy.get('#email'),
+    submitBtn: () => cy.get('.c-eXJNdE-cZntuR-desktop-true > .c-kDQqQr'),
+    description: () => cy.get('.c-hRNdXV > .c-deTVua'),
+    privacyPolicyLink: () => cy.get('[href="/privacy-policy"] > .c-khZXrc > .c-PJLV'),
+    termsAndCondition: () => cy.get('[href="/terms-and-conditions"] > .c-khZXrc > .c-PJLV')
+  
   };
 
   visit() {
