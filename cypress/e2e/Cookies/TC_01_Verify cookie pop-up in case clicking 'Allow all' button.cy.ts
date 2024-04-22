d5f5cc36f588be1a93a02573e6e7bc3065f102f6
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-22 17:28:45 +0200

    Fix1

--- cypress/e2e/Cookies/TC_01_Verify cookie pop-up in case clicking 'Allow all' button.cy.ts
+++ cypress/e2e/Cookies/TC_01_Verify cookie pop-up in case clicking 'Allow all' button.cy.ts
@@ -11,21 +11,21 @@ it("TC_01_Verify cookie pop-up in case clicking 'Allow all' button", () => {
     .should("have.text", "Telnyx Cookie Policy");
   cy.go("back");
 
-  cy.wait(4000)
+  mainPage.cookiesElements.cookiesSettingsBtn().should("be.visible");
   mainPage.cookiesElements.cookiesSettingsBtn().click({ force: true });
   mainPage.cookiesElements.cookiesSettingsModal().should("be.visible");
   mainPage.cookiesElements.cookiesSettingsCloseBtn().click({ force: true });
 
   mainPage.cookiesElements.cookieModal().should("be.visible");
-  
+
   mainPage.cookiesElements.cookiesSettingsBtn().click({ force: true });
 
-  // mainPage.cookiesElements.allowAllBtn().click({ force: true });
-  // mainPage.cookiesElements.cookiesSettingsIcon().should("be.visible");
+  mainPage.cookiesElements.allowAllBtn().click({ force: true });
+  mainPage.cookiesElements.cookiesSettingsIcon().should("be.visible");
 
-  // mainPage.cookiesElements.cookiesSettingsIcon().click({ force: true });
+  mainPage.cookiesElements.cookiesSettingsIcon().click({ force: true });
 
-  // mainPage.cookiesElements.performanceCookies().should("be.checked");
-  // mainPage.cookiesElements.functionalCookies().should("be.checked");
-  // mainPage.cookiesElements.targettingCookies().should("be.checked");
+  mainPage.cookiesElements.performanceCookies().should("be.checked");
+  mainPage.cookiesElements.functionalCookies().should("be.checked");
+  mainPage.cookiesElements.targettingCookies().should("be.checked");
 });
