d463ed62f86f8c38ae9793bae30fc98c49ed6b8e
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-22 17:10:24 +0200

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,22 +9,23 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on(
-        'after:spec',
-        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-          if (results && results.video) {
-            // Do we have failures for any retry attempts?
-            const failures = results.tests.some((test) =>
-              test.attempts.some((attempt) => attempt.state === 'failed')
-            )
-            if (!failures) {
-              // delete the video if the spec passed and no tests retried
-              fs.unlinkSync(results.video)
-            }
-          }
-        }
-      )
+      // on(
+      //   'after:spec',
+      //   (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+      //     if (results && results.video) {
+      //       // Do we have failures for any retry attempts?
+      //       const failures = results.tests.some((test) =>
+      //         test.attempts.some((attempt) => attempt.state === 'failed')
+      //       )
+      //       if (!failures) {
+      //         // delete the video if the spec passed and no tests retried
+      //         fs.unlinkSync(results.video)
+      //       }
+      //     }
+      //   }
+      // )
     },
-    baseUrl: "https://telnyx.com",   
+    baseUrl: "https://telnyx.com",  
+    video: true 
   },
 });
