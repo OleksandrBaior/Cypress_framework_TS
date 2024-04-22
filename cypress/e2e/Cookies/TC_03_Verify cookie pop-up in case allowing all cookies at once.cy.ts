0602b92fcc1d71015c4273cb9db2eeb2be01be5d
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-22 19:05:50 +0200

    fix 6

--- cypress/e2e/Cookies/TC_03_Verify cookie pop-up in case allowing all cookies at once.cy.ts
+++ cypress/e2e/Cookies/TC_03_Verify cookie pop-up in case allowing all cookies at once.cy.ts
@@ -4,9 +4,7 @@ import mainPage from "../../pages/main.page";
 it("TC_03_Verify cookie pop-up in case allowing all cookies at once", () => {
   cy.getCookies().should("be.empty");
   mainPage.visit();
-  cy.wait(3000);
   mainPage.cookiesElements.acceptAllBtn().click();
   mainPage.cookiesElements.cookieModal().should("not.visible");
-  mainPage.cookiesElements.cookiesSettingsIcon().should("be.visible");
   cy.getCookies().should("have.length", 7);
 });
