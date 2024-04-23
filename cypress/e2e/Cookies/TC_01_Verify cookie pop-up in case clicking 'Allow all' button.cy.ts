3691134c89cfcf6acdef191e334a08f06d24f447
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-23 20:26:04 +0200

    fix TC_01

--- cypress/e2e/Cookies/TC_01_Verify cookie pop-up in case clicking 'Allow all' button.cy.ts
+++ cypress/e2e/Cookies/TC_01_Verify cookie pop-up in case clicking 'Allow all' button.cy.ts
@@ -18,6 +18,7 @@ it("TC_01_Verify cookie pop-up in case clicking 'Allow all' button", () => {
   mainPage.cookiesElements.cookieModal().should("be.visible");
   mainPage.cookiesElements.cookiesSettingsBtn().click({ force: true });
 
+  mainPage.cookiesElements.allowAllBtn().should("not.be.visible");
   mainPage.cookiesElements.allowAllBtn().click({ force: true });
   mainPage.cookiesElements.cookieModal().should("not.be.visible");
 });
