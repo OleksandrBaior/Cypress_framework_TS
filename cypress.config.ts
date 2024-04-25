712bf5937fb941e58d164b7466a230b67550ea87
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-25 19:42:41 +0200

    update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,6 @@
 import cypress, { defineConfig } from "cypress";
 const { allureCypress } = require("allure-cypress/reporter");
+import fs from "fs";
 
 export default defineConfig({
   retries: {
@@ -7,35 +8,35 @@ export default defineConfig({
     openMode: 0,
   },
   e2e: {
-  
     setupNodeEvents(on, config) {
       allureCypress(on, {
         resultsDir: "./reportAllure/allure-results",
       });
 
-  
-      // on(
-      //   'after:spec',
-      //   (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-      //     if (results  && results.video) {
-      //       // Do we have failures for any retry attempts?
-      //       const failures = results.tests.some((test) =>
-      //         test.attempts.some((attempt) => attempt.state === 'failed')
-      //       )
-      //       if (!failures) {
-      //         // delete the video if the spec passed and no tests retried
-      //         fs.unlinkSync(results.video)
-      //       }
-      //     }
-      //   }
-      // )
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed")
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
     },
-   
+
     viewportWidth: 1536,
     viewportHeight: 960,
 
     baseUrl: "https://telnyx.com",
     video: true,
+    screenshotsFolder: "cypress/screenshots",
+    screenshotOnRunFailure: true,
 
     defaultCommandTimeout: 5000,
     execTimeout: 80000,
