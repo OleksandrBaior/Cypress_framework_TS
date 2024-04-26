1f5042484575480fc14556e0d46fcf417299ff80
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-26 16:55:53 +0200

    fix

--- cypress/e2e/Shop page/TC_07_Verify adding and deleting item from the shoppoing card.cy.ts
+++ cypress/e2e/Shop page/TC_07_Verify adding and deleting item from the shoppoing card.cy.ts
@@ -1,28 +1,16 @@
 /// <reference types="cypress" />
 import shopPage from "../../pages/shop.page";
 import constants from "../../../resourcers/constants.json";
-import * as allure from "allure-cypress";
 
 it("TC_07_Verify adding and deleting item from the shoppoing card", () => {
-  allure.feature("Shop page");
-
-  allure.step("Open the shop page", () => {
-    shopPage.visit();
-    shopPage.homeTab.featuredProducts().should("be.visible");
-  });
-  allure.step("Add first item from the list to shopping cart", () => {
-    shopPage.homeTab.firstItem().click();
-  });
-  allure.step("Verify that item is added to shopping cart", () => {
-    shopPage.yourCart.shoppingCart().should("be.visible");
-    shopPage.yourCart.addedItem().should("be.visible");
-  });
-  allure.step("Delete item from shopping cart", () => {
-    shopPage.yourCart.deleteItemIcon().click();
-    shopPage.yourCart.emptyText().should("have.text", constants.shopPage.shoppingCart.emptyText);
-  });
-  allure.step("Click on continue button", () => {
-    shopPage.yourCart.continueShoppingBtn().click();
-    shopPage.yourCart.shoppingCart().should("not.be.visible");
-  });
+  shopPage.visit();
+  
+  shopPage.homeTab.featuredProducts().should("be.visible");
+  shopPage.homeTab.firstItem().click();
+  shopPage.yourCart.shoppingCart().should("be.visible");
+  shopPage.yourCart.addedItem().should("be.visible");
+  shopPage.yourCart.deleteItemIcon().click();
+  shopPage.yourCart.emptyText().should("have.text", constants.shopPage.shoppingCart.emptyText);
+  shopPage.yourCart.continueShoppingBtn().click();
+  shopPage.yourCart.shoppingCart().should("not.be.visible");
 });
