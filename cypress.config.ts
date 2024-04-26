10fa1a1be374b25232749292d48eab229540ca83
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-26 16:09:52 +0200

    Update

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ const { allureCypress } = require("allure-cypress/reporter");
 import fs from "fs";
 
 export default defineConfig({
+  projectId: '7wmxm7',
   chromeWebSecurity: false,
 
   retries: {
@@ -14,6 +15,22 @@ export default defineConfig({
       allureCypress(on, {
         resultsDir: "./reportAllure/allure-results",
       });
+
+      // on(
+      //   "after:spec",
+      //   (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+      //     if (results && results.video) {
+      //       // Do we have failures for any retry attempts?
+      //       const failures = results.tests.some((test) =>
+      //         test.attempts.some((attempt) => attempt.state === "failed")
+      //       );
+      //       if (!failures) {
+      //         // delete the video if the spec passed and no tests retried
+      //         fs.unlinkSync(results.video);
+      //       }
+      //     }
+      //   }
+      // );
     },
 
     viewportWidth: 1536,
