5b9c59d7007f37b3889968e2c464155bbc4130dc
OleksandrBaior <bayor.olexandr@gmail.com>
2024-04-26 15:33:41 +0200

    Update config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,22 +14,6 @@ export default defineConfig({
       allureCypress(on, {
         resultsDir: "./reportAllure/allure-results",
       });
-
-      // on(
-      //   "after:spec",
-      //   (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
-      //     if (results && results.video) {
-      //       // Do we have failures for any retry attempts?
-      //       const failures = results.tests.some((test) =>
-      //         test.attempts.some((attempt) => attempt.state === "failed")
-      //       );
-      //       if (!failures) {
-      //         // delete the video if the spec passed and no tests retried
-      //         fs.unlinkSync(results.video);
-      //       }
-      //     }
-      //   }
-      // );
     },
 
     viewportWidth: 1536,
